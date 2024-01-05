633485f78c27f9db6da02d4377ef2110fc06fb90
Bhanu-code <bhanuchowhan652@gmail.com>
2024-01-05 10:53:55 +0530

    new skills added

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,26 +1,36 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 const skills = [
-  { skill: "Java", path:'/icons/java.png' },
-  { skill: "Spring/SpringBoot", path:'/icons/springboot.png' },
-  { skill: "Python", path:'/icons/python.png' },
-  { skill: "Git/Github", path:'/icons/github.png' },
-  { skill: "JavaScript", path:'/icons/javascript.png' },
-  { skill: "TypeScript", path:'/icons/typescript.png' },
-  { skill: "Bootstrap", path:'/icons/bootstrap.png' },
-  { skill: "Tailwind CSS", path:'/icons/tailwind.png' },
-  { skill: "Ant-Design", path:'/icons/antdesign.png' },
-  { skill: "Material-UI", path:'/icons/materialui.png' },
-  { skill: "React", path:'/icons/reactjs.png' },
-  { skill: "Next.js", path:'/icons/nextjs.png' },
-  { skill: "Node.js", path:'/icons/nodejs.png' },
-  { skill: "MongoDB", path:'/icons/mongodb.png'},
-  { skill: "MySQL", path:'/icons/mysql.png'},
-  { skill: "Linux", path:'/icons/linux.png' },
-  { skill: "Docker", path:'/icons/docker.png' },
-  { skill: "AWS Cloud", path:'/icons/aws.png'}
-]
+  { skill: "Java", path: "/icons/java.png", type: "language" },
+  {
+    skill: "Spring/SpringBoot",
+    path: "/icons/springboot.png",
+    type: "backend",
+  },
+  { skill: "Python", path: "/icons/python.png", type: "language" },
+  { skill: "Golang", path: "/icons/golang.png", type: "language" },
+  { skill: "Git/Github", path: "/icons/github.png", type: "operations" },
+  { skill: "JavaScript", path: "/icons/javascript.png", type: "language" },
+  { skill: "TypeScript", path: "/icons/typescript.png", type: "language" },
+  { skill: "Bootstrap", path: "/icons/bootstrap.png", type: "frontend" },
+  { skill: "Tailwind CSS", path: "/icons/tailwind.png", type: "frotnend" },
+  { skill: "Ant-Design", path: "/icons/antdesign.png", type: "frontend" },
+  { skill: "Material-UI", path: "/icons/materialui.png", type: "frontend" },
+  { skill: "React", path: "/icons/reactjs.png", type: "frontend" },
+  { skill: "Next.js", path: "/icons/nextjs.png", type: "frontend" },
+  { skill: "Node.js", path: "/icons/nodejs.png", type: "backend" },
+  { skill: "MongoDB", path: "/icons/mongodb.png", type: "backend" },
+  { skill: "MySQL", path: "/icons/mysql.png", type: "backend" },
+  { skill: "Linux", path: "/icons/linux.png", type: "operations" },
+  { skill: "Jenkins", path: "/icons/jenkins.png", type: "operations" },
+  { skill: "Ansible", path: "/icons/ansible.png", type: "operations" },
+  { skill: "CircleCI", path: "/icons/circleci.png", type: "operations" },
+  { skill: "Docker", path: "/icons/docker.png", type: "operations" },
+  { skill: "Kubernetes", path: "/icons/kubernetes.png", type: "operations" },
+  { skill: "Terraform", path: "/icons/terraform.png", type: "operations" },
+  { skill: "AWS Cloud", path: "/icons/aws.png", type: "operations" },
+];
 
 const AboutSection = () => {
   return (
@@ -37,24 +47,25 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Bhanu and I am {" "}
+              Hi, my name is Bhanu and I am{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer and aspiring SRE(site reliability engineer)
-              based in Kolkata, WB.
+              <span className="font-bold">{" driven"}</span> software engineer
+              and aspiring SRE(site reliability engineer) based in Kolkata, WB.
             </p>
             <br />
             <p>
-              I graduated from Vidyasagar University, Kolkata in 2021
-              with a BS in Computer Application and have been worked at Cognizant. Currently I am persuing MS in
-              Computer Application at Chandigarh University.
+              I graduated from Vidyasagar University, Kolkata in 2021 with a BS
+              in Computer Application and have been worked at Cognizant.
+              Currently I am persuing MS in Computer Application at Chandigarh
+              University.
             </p>
             <br />
             <p>
               I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to writing blogs,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
+              From reading, playing sports, traveling, to writing blogs, I am
+              always seeking new experiences and love to keep myself engaged and
+              learning new things.
             </p>
             <br />
             <p>
@@ -70,35 +81,130 @@ const AboutSection = () => {
           </div>
           <div className="text-center md:w-1/2 md:text-left">
             <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+
+            <h3 className="font-bold">Languages</h3>
             <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
               {skills.map((item, idx) => {
                 return (
                   <>
-                   <p
-                    key={idx}
-                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
-                  >
-                    {item.skill}
-                    &nbsp;
-                  <img src={item.path} alt="skill_icon" style={{ width:'20px', height:'20px', display:'inline' }} />
-                  </p>
+                    {item.type === "language" && (
+                      <p
+                        key={idx}
+                        className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                      >
+                        {item.skill}
+                        &nbsp;
+                        <img
+                          src={item.path}
+                          alt="skill_icon"
+                          style={{
+                            width: "20px",
+                            height: "20px",
+                            display: "inline",
+                          }}
+                        />
+                      </p>
+                    )}
                   </>
-                 
-                )
+                );
               })}
             </div>
-            <Image
+
+            <h3 className="font-bold">Frontend</h3>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <>
+                    {item.type === "frontend" && (
+                      <p
+                        key={idx}
+                        className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                      >
+                        {item.skill}
+                        &nbsp;
+                        <img
+                          src={item.path}
+                          alt="skill_icon"
+                          style={{
+                            width: "20px",
+                            height: "20px",
+                            display: "inline",
+                          }}
+                        />
+                      </p>
+                    )}
+                  </>
+                );
+              })}
+            </div>
+
+            <h3 className="font-bold">Backend</h3>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <>
+                    {item.type === "backend" && (
+                      <p
+                        key={idx}
+                        className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                      >
+                        {item.skill}
+                        &nbsp;
+                        <img
+                          src={item.path}
+                          alt="skill_icon"
+                          style={{
+                            width: "20px",
+                            height: "20px",
+                            display: "inline",
+                          }}
+                        />
+                      </p>
+                    )}
+                  </>
+                );
+              })}
+            </div>
+
+            <h3 className="font-bold">Operations</h3>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <>
+                    {item.type === "operations" && (
+                      <p
+                        key={idx}
+                        className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                      >
+                        {item.skill}
+                        &nbsp;
+                        <img
+                          src={item.path}
+                          alt="skill_icon"
+                          style={{
+                            width: "20px",
+                            height: "20px",
+                            display: "inline",
+                          }}
+                        />
+                      </p>
+                    )}
+                  </>
+                );
+              })}
+            </div>
+            {/* <Image
               src="/developer.jpg"
               alt="developer_image"
               width={350}
               height={350}
               className="hidden md:block md:relative md:bottom-0 my-3 md:left-32 md:z-0"
-            />
+            /> */}
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
+              MERN/LAMP Stack Developer{" "}
             </span>
             based in Kolkata, WB. Working towards creating software that
             makes life easier and more meaningful.
